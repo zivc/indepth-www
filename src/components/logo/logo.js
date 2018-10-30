961a6a13da128a8bf2c81513dc97e166d179aed6
Ash Taylor <a@zi.vc>
2018-10-30 23:36:59 +0000

    trying out logo

---
+++ src/components/logo/logo.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import * as styles from './logo.module.scss';
+import img from '../../images/idbg.png';
+
+const Logo = () => (
+  <div className={styles.glob}>
+    <div className={styles.wrapper}>
+      <div className={styles.bars} />
+      <div className={styles.box} />
+    </div>
+  </div>
+)
+
+export default Logo
