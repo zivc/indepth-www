5fd24d5c1a7fc828e963cf3186657a6a4f8a5d11
Ash Taylor <a@zi.vc>
2018-10-31 00:08:48 +0000

    simple

--- src/components/logo/logo.js
+++ src/components/logo/logo.js
@@ -1,14 +1,44 @@
 import React from 'react'
-import * as styles from './logo.module.scss';
-import img from '../../images/idbg.png';
+import * as styles from './logo.module.scss'
 
-const Logo = () => (
-  <div className={styles.glob}>
-    <div className={styles.wrapper}>
-      <div className={styles.bars} />
-      <div className={styles.box} />
-    </div>
-  </div>
-)
+class Logo extends React.Component {
+  render() {
+    return (
+      <div className={styles.glob}>
+        <div className={styles.wrapper}>
+          <div className={styles.bars}/>
+          <div className={styles.boxTransformer}>
+            <div className={styles.box}/>
+          </div>
+        </div>
+      </div>);
+  }
 
-export default Logo
+  componentDidMount() {
+    window.addEventListener('mousemove', Logo.onMousemove.bind(this));
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('mousemove', Logo.onMousemove.bind(this));
+  }
+
+  static onMousemove(e) {
+    const x = e.pageX - (window.innerWidth / 2);
+    const y = e.pageY - (window.innerHeight / 2);
+    Logo.doAwesome(x, y, 0.1, `.${styles.boxTransformer}`);
+    Logo.doAwesome(x, y, 0.05, `.${styles.bars}`);
+  }
+
+  static doAwesome(mouseX,mouseY,sensitivityMultiplier = 1,selector) {
+    const ele = document.querySelectorAll(selector);
+    if (!ele.length) return;
+
+    const div = ele[0];
+
+    const transform = `translate(${mouseX * sensitivityMultiplier}px, ${mouseY * sensitivityMultiplier}px)`;;
+    div.style.webkitTransform = transform;
+    div.style.transform = transform;
+  }
+}
+
+export default Logo;
